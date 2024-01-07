9fa82c0becbc8f1791c5b747ad1ffd3afe5f801a
Peetah <peterleemartin@gmail.com>
2024-01-07 11:34:53 -0600

    forgot to make a branch. saving and creating a branch

--- Assets/script.js
+++ Assets/script.js
@@ -1,3 +1,10 @@
+const currentDay = dayjs();
+$('#currentDay').text(currentDay.format('dddd, MMMM D YYYY'));
+
+
+
+
+
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
@@ -10,13 +17,14 @@ $(function () {
   // useful when saving the description in local storage?
   //
   // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
+  // block by comparing the id to the current hour. 
+  // HINTS: How can the id attribute of each time-block be used to conditionally add or remove the past, present, and future classes?  
+  //
+  // How can Day.js be used to get the current hour in 24-hour time? - H or HH
   //
   // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
+  // the values of the corresponding textarea elements. 
+  // HINT: How can the id
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
